2d69bc4561a2bfee1dceede7c3bcef0a985700ca
root <root@AsusA1400-otong>
2023-07-26 16:07:07 +0700

    add: sample multi ssh

---
+++ ssh-multi-server.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+
+array set remote_array {
+	root@192.168.8.110 getpass
+	klapertart@192.168.8.97 getpass
+}
+
+
+foreach {remote pass} [array get remote_array] {
+
+	log_file -a ssh-multiserver.log
+
+	spawn ssh $remote
+
+	expect {
+		"Are you sure*" {
+			send "yes\r"
+			exp_continue
+		}
+		"*password:" {
+			send "$pass\r"
+		}
+	}
+
+	expect {
+		"$ " {
+			send "pwd\r"
+			expect "$ "
+		}		
+		"#" {
+			send "pwd\r"
+			expect "#"
+		}
+	}
+
+	log_file
+
+	send "exit\r"
+	expect eof
+}
